f8650987f4cf99f2c868339f8ceff3069d827cdf
andrefangeloni <andrefangeloni@gmail.com>
2020-08-16 00:46:45 -0300

    feature(main): Added new style on Character card, implemented rule to add or remove favorite on local storage

--- marvel/src/screens/Main/index.tsx
+++ marvel/src/screens/Main/index.tsx
@@ -3,10 +3,16 @@ import {
   View,
   Text,
   Image,
+  Alert,
   FlatList,
   SafeAreaView,
   ImageBackground,
+  TouchableOpacity,
 } from 'react-native';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import api from '../../services/api';
 
@@ -26,19 +32,49 @@ interface Character {
 
 const Main = () => {
   const [offset, setOffset] = useState<number>(0);
+  const [favorites, setFavorites] = useState<number[]>([]);
   const [characters, setCharacters] = useState<Character[]>([]);
 
   useEffect(() => {
     loadCharacters();
+
+    loadFavorites();
   }, []);
 
   const loadCharacters = async () => {
-    const { data } = await api.get(
-      `/characters?ts=${timestamp}&apikey=${publicKey}&hash=${hash}&offset=${offset}`,
-    );
-    console.log(data.data.results);
-    setCharacters((characters) => [...characters, ...data.data.results]);
-    setOffset((offset) => offset + 20);
+    try {
+      const { data } = await api.get(
+        `/characters?ts=${timestamp}&apikey=${publicKey}&hash=${hash}&offset=${offset}`,
+      );
+
+      setCharacters((characters) => [...characters, ...data.data.results]);
+      setOffset((offset) => offset + 20);
+    } catch (err) {
+      Alert.alert(err.message);
+    }
+  };
+
+  const loadFavorites = async () => {
+    const savedFavorites = await AsyncStorage.getItem('favorites');
+    if (savedFavorites !== null) {
+      setFavorites(JSON.parse(savedFavorites));
+    }
+  };
+
+  const onToggleFavorite = async (id: number) => {
+    const alreadySelected = favorites.findIndex((item) => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = favorites.filter((item) => item !== id);
+      setFavorites(filteredItems);
+      await AsyncStorage.setItem('favorites', JSON.stringify(filteredItems));
+    } else {
+      setFavorites([...favorites, id]);
+      await AsyncStorage.setItem(
+        'favorites',
+        JSON.stringify([...favorites, id]),
+      );
+    }
   };
 
   return (
@@ -70,9 +106,27 @@ const Main = () => {
                   }}
                 />
               </View>
-              <View>
-                <Text style={styles.title}>{item.name}</Text>
-                <Text style={styles.description}>{item.description}</Text>
+              <View style={styles.descriptionContainer}>
+                <Text style={styles.title} numberOfLines={2}>
+                  {item.name}
+                </Text>
+                <Text style={styles.description} numberOfLines={2}>
+                  {item.description}
+                </Text>
+
+                <View style={styles.detailsContainer}>
+                  <TouchableOpacity style={styles.detailsButton}>
+                    <Text style={styles.detailsText}>Details</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={() => onToggleFavorite(item.id)}>
+                    <Icon
+                      name={favorites.includes(item.id) ? 'star' : 'star-o'}
+                      size={26}
+                      color="#f78f3f"
+                    />
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           )}
--- marvel/src/screens/Main/styles.ts
+++ marvel/src/screens/Main/styles.ts
@@ -17,29 +17,51 @@ const styles = StyleSheet.create({
   },
   cardContainer: {
     flexDirection: 'row',
-    padding: 20,
+    height: 150,
     marginBottom: 20,
     borderRadius: 6,
     backgroundColor: '#fff',
   },
   imageContainer: {
-    marginEnd: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: '100%',
+    width: '30%',
   },
   image: {
-    height: 80,
-    width: 80,
-    borderRadius: 40,
+    height: '100%',
+    width: '100%',
+    borderTopLeftRadius: 6,
+    borderBottomLeftRadius: 6,
+  },
+  descriptionContainer: {
+    flex: 1,
+    padding: 10,
   },
   title: {
-    marginBottom: 20,
     fontSize: 20,
     textAlign: 'center',
     fontWeight: 'bold',
   },
   description: {
-    color: '#666'
+    flex: 1,
+    marginVertical: 10,
+    color: '#666',
+  },
+  detailsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderRadius: 4,
+  },
+  detailsButton: {
+    paddingVertical: 4,
+    paddingHorizontal: 30,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#518cca',
+    backgroundColor: '#518cca',
+  },
+  detailsText: {
+    color: '#fff',
   },
 });
 
