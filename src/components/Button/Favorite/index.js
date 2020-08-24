24513ec11f22138c0e66781fcd4f2cf08fb94840
mariisabellaz <marilia.isabellaz@gmail.com>
2020-08-24 06:43:41 -0300

    new corrections

--- src/components/Button/Favorite/index.js
+++ src/components/Button/Favorite/index.js
@@ -1,18 +1,22 @@
 import React, {useState, useEffect} from 'react';
+import {useDispatch} from "react-redux";
 import { FontAwesome } from '@expo/vector-icons';
 import {Wrapper} from "./styles";
 import {colors} from "../../../theme";
+import {postFavorite} from "../../../redux/actions";
 
-const Favorite = ({onPress}) => {
+const Favorite = ({onPress, comics_id}) => {
 
     const [click, setClick] = useState(false);
+    const dispatch = useDispatch();
 
     function toggleClick() {
         setClick(!click)
     }
 
+
     return (
-        <Wrapper onPress={toggleClick}>
+        <Wrapper onPress={toggleClick()}>
             <FontAwesome name={click ? "heart" : "heart-o"} size={30} color={colors.black}/>
         </Wrapper>
     );
