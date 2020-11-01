16fad58b325c98a13605c6e0dea7c2990790ef0a
Renan Vital <renanbreno@gmail.com>
2020-10-31 21:43:15 -0300

    refactor: remove types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,22 +1,4 @@
 declare namespace Marvel {
-  interface CharacterDataWrapper {
-    code?: number; // The HTTP status code of the returned result.
-    status?: string; // A string description of the call status.
-    copyright?: string; // The copyright notice for the returned result.
-    attributionText?: string; // The attribution notice for this result. Please display either this notice or the contents of the attributionHTML field on all screens which contain data from the Marvel Comics API.
-    attributionHTML?: string; // An HTML representation of the attribution notice for this result. Please display either this notice or the contents of the attributionText field on all screens which contain data from the Marvel Comics API.
-    data?: CharacterDataContainer; // The results returned by the call.
-    etag?: string; // A digest value of the content returned by the call.
-  }
-
-  interface CharacterDataContainer {
-    offset?: number; // The requested offset (number of skipped results) of the call.
-    limit?: number; // The requested result limit.
-    total?: number; // The total number of resources available given the current filter set.
-    count?: number; // The total number of results returned by this call.
-    results?: Character[]; // The list of characters returned by the call.
-  }
-
   interface Character {
     id?: number; // The unique ID of the character resource.
     name?: string; // The name of the character.
