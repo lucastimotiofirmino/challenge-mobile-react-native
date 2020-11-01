c0795dd9a3f45e20365647053dabd1f9d852ad6d
Renan Vital <renanbreno@gmail.com>
2020-10-31 21:43:55 -0300

    feat: get characters resuls from marvel api

--- src/components/Characters/index.tsx
+++ src/components/Characters/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Container, Title} from './styles';
+import api from '../../services/api';
 
-const App = () => {
+type CharactersState = Marvel.Character[] | undefined;
+
+const Characters = () => {
+  const [characters, setCharacters] = useState<CharactersState>([]);
+
+  useEffect(() => {
+    async function getCharacters(): Promise<CharactersState> {
+      const {
+        data: {data},
+      } = await api.get('/characters');
+      return data.results;
+    }
+
+    getCharacters()
+      .then((results) => {
+        setCharacters(results);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <Container>
-      <Title>MARVEL APP</Title>
+      <Title>MARVEL APP: {characters?.length}</Title>
     </Container>
   );
 };
 
-export default App;
+export default Characters;
