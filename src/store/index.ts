e6eccb9598426ff80c4b904b28c50d13250a28f3
Italo Mota <italomota123@gmail.com>
2021-02-28 11:17:04 -0300

    adiciona redux e sagas a configuração do store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+import { combineReducers } from 'redux'
+
+import rootSaga from '../sagas'
 
 // @ts-ignore
 import Reactotron from '../config/Reactotron'
 
-const store = configureStore({
-  reducer: () => {},
-  // @ts-ignore
-  enhancers: [Reactotron.createEnhancer()]
+import charactersReducers from './characters'
+
+const uiReducers = combineReducers({
+  characters: charactersReducers.ui
 })
+
+const entityReducers = combineReducers({
+  characters: charactersReducers.entity
+})
+
+export const rootReducer = combineReducers({
+  ui: uiReducers,
+  entity: entityReducers
+})
+
+// @ts-ignore
+const sagaMonitor = Reactotron.createSagaMonitor()
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor })
+
+const store = configureStore({
+  reducer: rootReducer,
+  // @ts-ignore
+  enhancers: [Reactotron.createEnhancer()],
+  middleware: [sagaMiddleware]
+})
+
+sagaMiddleware.run(rootSaga)
 
 export default store
