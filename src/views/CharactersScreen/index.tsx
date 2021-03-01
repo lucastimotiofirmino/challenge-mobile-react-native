a4a044810ba1a4b473b385e93276fd7803e09494
Italo Mota <italomota123@gmail.com>
2021-03-01 01:34:52 -0300

    implementa filtro por favoritos

--- src/views/CharactersScreen/index.tsx
+++ src/views/CharactersScreen/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useMemo } from 'react'
-import { Text, FlatList, TouchableOpacity, View } from 'react-native'
+import { Text, FlatList, TouchableOpacity, View, SafeAreaView } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigation } from '@react-navigation/native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
@@ -15,10 +15,11 @@ const CharactersScreen = () => {
 
   const navigation = useNavigation()
 
-  const characters = useSelector((state): Characters => state.entity?.characters || [])
+  const characters = useSelector((state): Characters => state.entity.characters || [])
   const fetching = useSelector((state): boolean => state.ui.characters.fetching)
 
   const [favoritesCharacters, setFavoritesCharacters] = useState<Characters>([])
+  const [favoritesCharactersVisible, setfavoritesCharactersVisible] = useState<boolean>(false)
 
   const fetchCharacters = () => {
     if (fetching) {
@@ -64,6 +65,14 @@ const CharactersScreen = () => {
     await AsyncStorage.setItem(STORAGE_KEY_FAVORITES, JSON.stringify(favoritesCharacters))
   }
 
+  const filteredCharacters = useMemo(() => {
+    if (favoritesCharactersVisible) {
+      return favoritesCharacters
+    }
+
+    return characters
+  }, [favoritesCharactersVisible, favoritesCharacters, characters])
+
   useEffect(() => {
     fetchCharacters()
     fetchFavoritesCharacters()
@@ -89,14 +98,19 @@ const CharactersScreen = () => {
   )
 
   return (
-    <FlatList
-      contentContainerStyle={{ alignItems: 'center' }}
-      data={characters}
-      renderItem={renderItem}
-      keyExtractor={item => String(item.id)}
-      onEndReached={fetchCharacters}
-      onEndReachedThreshold={0.1}
-    />
+    <SafeAreaView>
+      <TouchableOpacity onPress={() => setfavoritesCharactersVisible(!favoritesCharactersVisible)}>
+        <Text>{favoritesCharactersVisible ? 'Exibir todos' : 'Apenas favoritos'}</Text>
+      </TouchableOpacity>
+      <FlatList
+        contentContainerStyle={{ alignItems: 'center' }}
+        data={filteredCharacters}
+        renderItem={renderItem}
+        keyExtractor={item => String(item.id)}
+        onEndReached={fetchCharacters}
+        onEndReachedThreshold={0.1}
+      />
+    </SafeAreaView>
   )
 }
 
