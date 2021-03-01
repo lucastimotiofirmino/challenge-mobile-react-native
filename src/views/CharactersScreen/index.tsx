b0e17a3c7767a4b6f37062ae886a4bfde88d18ab
Italo Mota <italomota123@gmail.com>
2021-03-01 01:10:08 -0300

    permite salvar personagem como favorito

--- src/views/CharactersScreen/index.tsx
+++ src/views/CharactersScreen/index.tsx
@@ -1,19 +1,24 @@
-import React, { useEffect } from 'react'
-import { Text, FlatList, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState, useMemo } from 'react'
+import { Text, FlatList, TouchableOpacity, View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigation } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { Character, Characters } from '../../entities'
 
 import { charactersActions } from '../../store/characters'
 
 const CharactersScreen = () => {
+  const STORAGE_KEY_FAVORITES = 'favoritesCharacters'
+
   const dispatch = useDispatch()
 
   const navigation = useNavigation()
 
   const characters = useSelector((state): Characters => state.entity?.characters || [])
   const fetching = useSelector((state): boolean => state.ui.characters.fetching)
+
+  const [favoritesCharacters, setFavoritesCharacters] = useState<Characters>([])
 
   const fetchCharacters = () => {
     if (fetching) {
@@ -28,16 +33,59 @@ const CharactersScreen = () => {
     navigation.navigate('CharacterDetails')
   }
 
+  const fetchFavoritesCharacters = async () => {
+    const favoritesCharactersStorage = await AsyncStorage.getItem(STORAGE_KEY_FAVORITES)
+    const favoritesCharacters: Characters = !!favoritesCharactersStorage ? JSON.parse(favoritesCharactersStorage) : []
+
+    setFavoritesCharacters(favoritesCharacters)
+  }
+
+  const favoritesCharactersIds = useMemo(() => (
+    favoritesCharacters.map(favoriteCharacter => favoriteCharacter.id)
+  ), [favoritesCharacters])
+
+  const onPressFavorite = (character: Character) => {
+    favoritesCharactersIds.includes(character.id)
+      ? removeFavoriteCharacter(character.id)
+      : addFavoriteCharacter(character)
+  }
+
+  const addFavoriteCharacter = async (character: Character) => {
+    setFavoritesCharacters([...favoritesCharacters, character])
+  }
+
+  const removeFavoriteCharacter = async (characterId: string) => {
+    const filteredFavoritesCharacters = favoritesCharacters.filter(favoriteCharacter => favoriteCharacter.id !== characterId)
+    
+    setFavoritesCharacters(filteredFavoritesCharacters)
+  }
+
+  const updateStorage = async () => {
+    await AsyncStorage.setItem(STORAGE_KEY_FAVORITES, JSON.stringify(favoritesCharacters))
+  }
+
   useEffect(() => {
     fetchCharacters()
+    fetchFavoritesCharacters()
   }, [])
 
+  useEffect(() => {
+    updateStorage()
+  }, [favoritesCharacters])
+
   const renderItem = ({ item }: { item: Character }) => (
-    <TouchableOpacity onPress={() => onPressCharacter(item.id)}>
-      <Text style={{ marginVertical: 5 }}>
-        {item.name}
-      </Text>
-    </TouchableOpacity>
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <TouchableOpacity onPress={() => onPressCharacter(item.id)}>
+        <Text style={{ marginVertical: 10 }}>
+          {item.name}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => onPressFavorite(item)}>
+        <Text>
+         {favoritesCharactersIds.includes(item.id) ? '(Remover favorito)' : '(Adicionar favorito)'}
+        </Text>
+      </TouchableOpacity>
+    </View>
   )
 
   return (
