e754aa50fdf67462706a5de8aedba157ea10087e
Italo Mota <italomota123@gmail.com>
2021-02-28 22:03:55 -0300

    implementa estrutura base da tela de detalhes de um personagem

---
+++ src/views/CharacterDetailsScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const CharacterDetailsScreen = () => {
+  return (
+    <View>
+      <Text>Character Details</Text>
+    </View>
+  )
+}
+
+export default CharacterDetailsScreen
