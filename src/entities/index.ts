c4b83e5b84e68385e0a349ef9e9f875b1404b285
Italo Mota <italomota123@gmail.com>
2021-02-28 23:16:28 -0300

    - implementa entidades para as séries - implementa entidade para os thumbnails

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,11 +1,24 @@
+interface Thumbnail {
+  path: string
+  extension: string
+}
+
 export interface Character {
   id: string
   name: string
   description: string
-  thumbnail: {
-    path: string
-    extension: string
-  }
+  thumbnail: Thumbnail
 }
 
 export type Characters = Array<Character>
+
+export interface Series {
+  id: number
+  title: string
+  description: string
+  startYear: number
+  endYear: number
+  thumbnail: Thumbnail
+}
+
+export type SeriesList = Array<Series>
