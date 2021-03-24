85fce4d6e23bbc189a3967089a429ef0511a4b96
Renato Rosseto <renato_rosseto_neto@hotmail.com>
2021-03-24 19:10:23 -0300

    Criando header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Image, Text } from 'react-native'
+
+import logoImg from '../../assets/logo/logo.png'
 
 import styles from './styles'
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>
-        Teste
-      </Text>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Filtro
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>Selecione um personagem para saber mais sobre séries e eventos</Text>
     </View>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,9 +1,30 @@
 import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 10,
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  title: {
+    fontSize: 30,
+    marginBottom: 16,
+    marginTop: 48,
+    color: '#13131a',
+    fontWeight: 'bold',
+  },
+
+  description: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#737380'
   }
 })
