a57a5e89e6afd4b0bdd429edfa9ae9a8f75e7550
Talles Barbosa <talles.barbosa@dellead.com>
2021-03-25 12:10:27 -0300

    Base project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Login from '~/views/Login';
+
+const App = () => <View style={{ flex: 1, backgroundColor: 'black' }} />;
+
+export default App;
