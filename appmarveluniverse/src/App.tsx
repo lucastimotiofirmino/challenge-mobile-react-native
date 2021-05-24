05dc4e1727a54c2835d014e5883c73a9e57c3dc0
SDamasceno.Dev <sdamasceno.dev@gmail.com>
2021-05-24 17:00:39 -0300

    Initial structure with routes defined

--- appmarveluniverse/src/App.tsx
+++ appmarveluniverse/src/App.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
-import { View } from 'react-native';
+import 'react-native-gesture-handler';
 
-const App: React.FC = () => <View />;
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#000" translucent />
+      <View style={{ flex: 1, backgroundColor: '#000' }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
+  );
+};
 
 export default App;
